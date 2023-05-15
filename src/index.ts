d92b6b411c89c7d1a82cc9cc278fdecf334e1f43
Cristian Campos <20213tn003@utez.edu.mx>
2023-05-15 14:03:22 -0600

    example

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express"; //ESModules
+
+import diaryRouter from './routes/diaries'
+// const express = require('express') ---> commonjs
+const app = express();
+app.use(express.json()); //transforma el req.body en json
+
+const PORT = 3000;
+app.get("/ping", (_req, res) => {
+  console.log("someone pinged here");
+  res.send("pong");
+});
+
+app.use('/api/diaries', diaryRouter)
+
+app.listen(PORT, () => console.log(`Server runnning on port ${PORT}`));
